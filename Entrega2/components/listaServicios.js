656b8ee8b0a65ebd6a2bde06fd3cc607f76ce662
mitooos <ma.hoyosr@uniandes.edu.co>
2020-09-24 18:35:38 -0500

    se completa el body de las cards de servicio

--- Entrega2/components/listaServicios.js
+++ Entrega2/components/listaServicios.js
@@ -1,10 +1,13 @@
 import { servicios as serviciosData } from "../Datos/servicio.js";
+import { data as proveedoresData } from "../Datos/listaProveedores.js";
 
 const renderServicios = (servicios) => {
   let cardDeck = document.createElement("div");
   cardDeck.classList.add("card-deck");
 
   servicios.forEach((item) => {
+    let proveedor = proveedoresData.find((ite) => ite.id == item.fk_proveedor);
+
     let card = document.createElement("div");
     card.classList.add("card");
 
@@ -16,10 +19,29 @@ const renderServicios = (servicios) => {
     cardTitle.innerHTML = item.nombre;
     cardBody.appendChild(cardTitle);
 
+    let cardSubTitle = document.createElement("h6");
+    cardSubTitle.classList.add("card-subtitle");
+    cardSubTitle.innerHTML = proveedor.ciudad;
+    cardBody.appendChild(cardSubTitle);
+
     let cardDescription = document.createElement("p");
     cardDescription.classList.add("card-text");
     cardDescription.innerHTML = item.descripcion;
     cardBody.appendChild(cardDescription);
+
+    let price = document.createElement("p");
+    price.classList.add("card-text");
+    price.innerHTML = item.precio_minimo;
+    cardBody.appendChild(price);
+
+    let linkProveedor = document.createElement("a");
+    linkProveedor.href = "";
+    linkProveedor.classList.add("card-link");
+    linkProveedor.innerHTML = "Contacto proveedor";
+    linkProveedor.addEventListener("click", () => {
+      console.log("llevame al detalle del proveedor");
+    });
+    cardBody.appendChild(linkProveedor);
 
     card.appendChild(cardBody);
 
