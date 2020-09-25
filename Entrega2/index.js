4ecef3193d17c75c2b312b9ffa032d7b74f85864
Daniel Galndo <d.galindo@uniandes.edu.co>
2020-09-24 21:06:04 -0500

    todo

--- Entrega2/index.js
+++ Entrega2/index.js
@@ -2,7 +2,7 @@ import { render as renderHome } from "./components/home.js";
 import { render as renderProveedores } from "./components/listaProveedores.js";
 import { render as renderCards } from "./components/cards.js";
 import { render as renderContratosClientes } from "./components/servicioContratado.js";
-
+import {render as renderCreateContract} from "./components/createContract.js";
 const body = document.getElementById("body");
 
 // initial paint
@@ -23,6 +23,7 @@ document.getElementById("contratosClientes").addEventListener("click", () => {
   renderContratosClientes(body);
 });
 
+document.getElementById("")
 // navbar modification
 
 const navbarList = document.getElementById("navbar-list");
@@ -47,14 +48,14 @@ const appendNavbarElement = (text, action) => {
 const renderClienteNavbar = () => {
   navbarList.innerHTML = "";
   appendNavbarElement("Clientes component 1", () => {
-    console.log("render component");
+    renderCreateContract(body);
   });
 };
 
 const renderProveedoresNavbar = () => {
   navbarList.innerHTML = "";
-  appendNavbarElement("Proveedor component 1", () =>
-    console.log("proveedor component 1")
+  appendNavbarElement("Crear contrato", () =>
+    renderCreateContract(body)
   );
 };
 
